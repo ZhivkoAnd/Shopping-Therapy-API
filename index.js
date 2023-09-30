5f2b4269b7b6c882adf3f67160a8dc2b25671293
ZhivkoAnd <Zhivko_Andreev@abv.bg>
2023-09-30 23:49:59 +0200

    a

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Endpoint to get the array of cities
+app.get('/cities', (req, res) => {
+  // Read the JSON file and send it as a response
+  const cities = require('./cities.json');
+  res.json(cities);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+// Export the Express API
+module.exports = app;
