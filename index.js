77bedc62a02a0d0de49a7888255905425bbbf392
ZhivkoAnd <Zhivko_Andreev@abv.bg>
2023-09-30 23:59:02 +0200

    a

--- index.js
+++ index.js
@@ -17,5 +17,21 @@ app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
+
+// Endpoint for specific city
+app.get('/cities/:id', (req, res) => {
+  // Read the JSON file and find the city by ID
+  const cities = require('./cities.json');
+  const cityId = parseInt(req.params.id);
+
+  const city = cities.find(city => city.id === cityId);
+
+  if (!city) {
+    return res.status(404).json({ message: 'City not found' });
+  }
+
+  res.json(city);
+});
+
 // Export the Express API
 module.exports = app;
